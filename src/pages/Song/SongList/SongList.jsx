541f5f24ae652e852a4da8efe025c224b3ceca72
TonyNguyen <truong.nn@amanotes.com>
2022-03-23 10:55:06 +0700

    add star

--- src/pages/Song/SongList/SongList.jsx
+++ src/pages/Song/SongList/SongList.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 
 // assets
 import ImageTextPlay from "assets/images/text-play.svg";
